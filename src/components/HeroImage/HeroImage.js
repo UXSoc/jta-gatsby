ff9f90046233d70b2202b498edce4241deb6b330
Xavi Ablaza <xlablaza@gmail.com>
2018-07-15 00:16:32 +0800

    Use wider HeroImage

--- src/components/HeroImage/HeroImage.js
+++ src/components/HeroImage/HeroImage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HeroImage = () => (
   <div className="container is-fullhd">
-    <img src="https://picsum.photos/1000/500" />
+    <img src="https://picsum.photos/1900/750" />
   </div>
 );
 

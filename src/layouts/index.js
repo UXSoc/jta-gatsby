cbbeafc763a6db841a42ea68b4c5509f548a214c
Xavi Ablaza <xlablaza@gmail.com>
2018-07-15 10:19:16 +0800

    Use HorizontalTimelineContent
    Move index out of application renderer

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,13 +3,6 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
 import Navbar from '../components/Navbar';
-import HeroBanner from '../components/HeroBanner';
-import HeroImage from '../components/HeroImage';
-import HeroDescription from '../components/HeroDescription';
-import ProcessTimeline from '../components/ProcessTimeline';
-import BulmaCard from '../components/BulmaCard';
-import Downloadables from '../components/Downloadables';
-import Footer from '../components/Footer';
 import './index.scss';
 
 const TemplateWrapper = ({ children }) => (
@@ -22,40 +15,7 @@ const TemplateWrapper = ({ children }) => (
       ]}
     />
     <Navbar />
-    <HeroBanner />
-    <HeroImage />
-    <HeroDescription
-      title="About the Junior Term Abroad Program"
-      description="The Junior Term Abroad Program is an opportunity that allows juniors of the Ateneo to study abroad for one semester. Previous participants have described their study-abroad experiences as both academically enriching and personally rewarding, an important period of growth and discovery in their college lives. Participants are expected to be Ateneo's unofficial student-ambassadors, helping build our relationships with our partner institutions abroad."
-    />
-    <ProcessTimeline title="The Process" />
-    <div className="section">
-      <div className="container">
-        <h4 className="title is-4">The Journey</h4>
-        <div className="columns">
-          <div className="column">
-            <BulmaCard
-              title="JTA Destinations & Universities"
-              description="Some quick example text to build on the card title and make up the bulk of the card's content."
-            />
-          </div>
-          <div className="column">
-            <BulmaCard
-              title="JTA Destinations & Universities"
-              description="Some quick example text to build on the card title and make up the bulk of the card's content."
-            />
-          </div>
-          <div className="column">
-            <BulmaCard
-              title="JTA Destinations & Universities"
-              description="Some quick example text to build on the card title and make up the bulk of the card's content."
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-    <Downloadables />
-    <Footer />
+    {children()}
   </div>
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,76 @@
 import React from 'react';
-import Link from 'gatsby-link';
+import HeroBanner from '../components/HeroBanner';
+import HeroImage from '../components/HeroImage';
+import HeroDescription from '../components/HeroDescription';
+import BulmaCard from '../components/BulmaCard';
+import Downloadables from '../components/Downloadables';
+import Footer from '../components/Footer';
+import GameInfo from '../resources/content';
+import HorizontalTimelineContent from '../components/HorizontalTimelineContent/HorizontalTimelineContent';
 
-const IndexPage = () => (
-  <div className="content">
-    <h1>
-      Hi people <i className="fas fa-exclamation-circle" />
-    </h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-    <div>
-      <Link to="/counter/">Counter</Link>
-    </div>
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 0, previous: 0 };
+  }
 
-    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
-      <h1>Richard Hamming on Luck</h1>
+  componentWillMount() {
+    this.data = GameInfo.map((game, index) => {
+      return {
+        date: game.date,
+        component: (
+          <div className="container" key={index}>
+            <h1>{`The Elder Scrolls ${index + 1}:`}</h1>
+            <h2>{game.subtitle}</h2>
+            <hr />
+            <p>{game.content}</p>
+            <hr />
+          </div>
+        ),
+      };
+    });
+  }
+  render() {
+    return (
       <div>
-        <p>
-          From Richard Hamming’s classic and must-read talk, “<a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-            You and Your Research
-          </a>”.
-        </p>
-        <blockquote>
-          <p>
-            There is indeed an element of luck, and no, there isn’t. The
-            prepared mind sooner or later finds something important and does it.
-            So yes, it is luck.{' '}
-            <em>
-              The particular thing you do is luck, but that you do something is
-              not.
-            </em>
-          </p>
-        </blockquote>
+        <HeroBanner />
+        <HeroImage />
+        <HeroDescription
+          title="About the Junior Term Abroad Program"
+          description="The Junior Term Abroad Program is an opportunity that allows juniors of the Ateneo to study abroad for one semester. Previous participants have described their study-abroad experiences as both academically enriching and personally rewarding, an important period of growth and discovery in their college lives. Participants are expected to be Ateneo's unofficial student-ambassadors, helping build our relationships with our partner institutions abroad."
+        />
+        <HorizontalTimelineContent
+          content={this.data} />
+        <div className="section">
+          <div className="container">
+            <h4 className="title is-4">The Journey</h4>
+            <div className="columns">
+              <div className="column">
+                <BulmaCard
+                  title="JTA Destinations & Universities"
+                  description="Some quick example text to build on the card title and make up the bulk of the card's content."
+                />
+              </div>
+              <div className="column">
+                <BulmaCard
+                  title="JTA Destinations & Universities"
+                  description="Some quick example text to build on the card title and make up the bulk of the card's content."
+                />
+              </div>
+              <div className="column">
+                <BulmaCard
+                  title="JTA Destinations & Universities"
+                  description="Some quick example text to build on the card title and make up the bulk of the card's content."
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <Downloadables />
+        <Footer />
       </div>
-      <p>Posted April 09, 2011</p>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default IndexPage;

d3751de68918af7ad04b72446fa58877caf8a3ce
Xavi Ablaza <xlablaza@gmail.com>
2018-07-16 09:25:41 +0800

    Add conditional rendering to HeroDescription

--- src/pages/about-jta.js
+++ src/pages/about-jta.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import SiteNavigator from '../components/SiteNavigator/SiteNavigator';
+import HeroImage from '../components/HeroImage';
+import HeroDescription from '../components/HeroDescription/HeroDescription';
 
 const PAGES = [
   {
@@ -18,6 +20,11 @@ const PAGES = [
 const AboutJta = () => (
   <div>
     <SiteNavigator title="Getting Started" pages={PAGES} />
+    <HeroImage />
+    <HeroDescription
+      title=""
+      description="The information below will give you an overview on what the Junior Term Abroad Program is about, the requirements to take part in this program, the costs, as well as notable student experiences. We look forward to working with you as you plan an amazing experience abroad!"
+    />
   </div>
 );
 

5f68f40a477ae143aace0c1433ba9d13adcb6c60
Xavi Ablaza <xlablaza@gmail.com>
2018-07-16 09:25:19 +0800

    Wrap SiteNavigator in hero div

--- src/components/SiteNavigator/SiteNavigator.js
+++ src/components/SiteNavigator/SiteNavigator.js
@@ -2,23 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SiteNavigator = props => (
-  <div className="container">
-    <div className="columns">
-      <div className="column">
-        <h2 className="title">{props.title}</h2>
-        <div className="site-navigator">
-          {props.pages.map((name, index) => {
-            return (
-              <div className="page-link" key={index}>
-                <span className="link-icon">
-                  <i className="fas fa-angle-right" />
-                </span>
-                <a className="name" href={name.link}>
-                  {name.title}
-                </a>
-              </div>
-            );
-          })}
+  <div className="hero">
+    <div className="hero-body">
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+            <h2 className="title">{props.title}</h2>
+            <div className="site-navigator">
+              {props.pages.map((name, index) => {
+                return (
+                  <div className="page-link" key={index}>
+                    <span className="link-icon">
+                      <i className="fas fa-angle-right" />
+                    </span>
+                    <a className="name" href={name.link}>
+                      {name.title}
+                    </a>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </div>
